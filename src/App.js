281e07e52e6696f1a198ce4b949c8e44d468ac82
ej.omictin <earl.june.o@gmail.com>
2023-11-15 16:06:36 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-native-web";
+import Rcon from "rcon";
+
+function App() {
+  return (
+    <>
+      <Button
+        title="Press me"
+      />
+    </>
+  );
+}
+
+export default App;
